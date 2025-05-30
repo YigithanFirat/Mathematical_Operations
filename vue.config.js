b9f20d6c7ad90f4fedfae076e64fbc94ecbc5bea
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-05-30 12:27:06 +0300

    Vue Config
    Vue Config düzenlendi.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig
-({
-    devServer:
-    {
-        proxy: 'http://localhost:8080',
-    },
-    transpileDependencies: true,
-    lintOnSave: false
-})
+const webpack = require('webpack');
+
+module.exports = defineConfig({
+  devServer: {
+    proxy: 'http://localhost:8080',
+  },
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
+});
